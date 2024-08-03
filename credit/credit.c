6ebec1b716f6467ca65e1d42fd161c0d4b42b2dd
YungBenKenobi <fleming.ash@gmail.com>
2024-08-03 14:03:10 +1000

    Update credit.c
    passing grade. needs more tidying.

--- credit/credit.c
+++ credit/credit.c
@@ -1,139 +1,153 @@
 #include <cs50.h>
 #include <stdio.h>
 
+int prodfinder(int x);
+
 int main(void)
 {
     // Take card number (cc) from user
-    long card = get_long("Card Number: ");
-
-    // Copy to placeholder integer
-    long cc = card;
+    long cc = get_long("Card Number: ");
 
     // Initialise counter = 0
     int counter = 0;
+
+    // Find digit values and places
+    int a = cc % 10; // cc % 10 to find last digit and save to int
+    cc = cc / 10;    // cc / 10 to reduce by order of magnitude
+    counter++;       // Digit counter ++
+    int b = cc % 10;
+    cc = cc / 10;
+    counter++;
+    int c = cc % 10;
+    cc = cc / 10;
+    counter++;
+    int d = cc % 10;
+    cc = cc / 10;
+    counter++;
+    int e = cc % 10;
+    cc = cc / 10;
+    counter++;
+    int f = cc % 10;
+    cc = cc / 10;
+    counter++;
+    int g = cc % 10;
+    cc = cc / 10;
+    counter++;
+    int h = cc % 10;
+    cc = cc / 10;
+    counter++;
+    int i = cc % 10;
+    cc = cc / 10;
+    counter++;
+    int j = cc % 10;
+    cc = cc / 10;
+    counter++;
+    int k = cc % 10;
+    cc = cc / 10;
+    counter++;
+    int l = cc % 10;
+    cc = cc / 10;
+    counter++;
+    int m = cc % 10;
+    cc = cc / 10;
+    counter++;
     int n = 0;
     int o = 0;
     int p = 0;
-
-    // Perform checksum validation
-
-    int a = cc % 10;
-    cc = cc / 10;
-    counter ++;
-    int b = cc % 10;
-    cc = cc / 10;
-    counter ++;
-    int c = cc % 10;
-    cc = cc / 10;
-    counter ++;
-    int d = cc % 10;
-    cc = cc / 10;
-    counter ++;
-    int e = cc % 10;
-    cc = cc / 10;
-    counter ++;
-    int f = cc % 10;
-    cc = cc / 10;
-    counter ++;
-    int g = cc % 10;
-    cc = cc / 10;
-    counter ++;
-    int h = cc % 10;
-    cc = cc / 10;
-    counter ++;
-    int i = cc % 10;
-    cc = cc / 10;
-    counter ++;
-    int j = cc % 10;
-    cc = cc / 10;
-    counter ++;
-    int k = cc % 10;
-    cc = cc / 10;
-    counter ++;
-    int l = cc % 10;
-    cc = cc / 10;
-    counter ++;
-    int m = cc % 10;
-    cc = cc/ 10;
-    counter ++;
-    if(cc > 9)
+    if (cc > 9)
     {
         n = cc % 10;
         cc = cc / 10;
-        counter ++;
+        counter++;
+        if (cc > 9)
+        {
+            o = cc % 10;
+            cc = cc / 10;
+            counter++;
+            if (cc > 0)
+            {
+                p = cc;
+                counter++;
+                cc = 0;
+            }
+        }
+        else if (cc < 10 && cc > 0)
+        {
+            o = cc;
+            counter++;
+            cc = 0;
+        }
     }
-    if(cc > 9)
+    else if (cc > 0)
     {
-        o = cc % 10;
-        cc = cc / 10;
-        counter ++;
+        n = cc;
+        counter++;
+        cc = 0;
     }
-    if(cc > 9)
+    // Multiply each second digit by 2. Split product integers and add together if necessary
+    int prodb = prodfinder(b);
+    int prodd = prodfinder(d);
+    int prodf = prodfinder(f);
+    int prodh = prodfinder(h);
+    int prodj = prodfinder(j);
+    int prodl = prodfinder(l);
+    int prodn = prodfinder(n);
+    int prodp = prodfinder(p);
+    int sum = a + prodb + c + prodd + e + prodf + g + prodh + i + prodj + k + prodl + m + prodn +
+              o + prodp;    // Sum all results together
+    int modulus = sum % 10; // Find modulo 10
+    if (modulus == 0)
     {
-        p = cc % 10;
-        cc = cc / 10;
-        counter ++;
-    }
-    else if(cc < 10 && cc > 0)
-    {
-        p = cc;
-        counter ++;
-    }
-    int prodx;
-    if(n > 0 && p > 0)
-    {
-        prodx = (int prodb  = b * 2) + (d * 2) + (f * 2) + (h * 2) + (j * 2) + (l * 2) + (n * 2) + (p * 2);
-    }
-    else if(n > 0)
-    {
-        prodx = (b * 2) + (d * 2) + (f * 2) + (h * 2) + (j * 2) + (l * 2) + (n * 2);
+        if (counter == 13 && m == 4)
+        {
+            printf("VISA\n");
+        }
+        else if (counter == 16 && p == 4)
+        {
+            printf("VISA\n");
+        }
+        else if (counter == 15 && o == 3)
+        {
+            if (n == 4 || n == 7)
+            {
+                printf("AMEX\n");
+            }
+            else
+            {
+                printf("INVALID\n");
+            }
+        }
+        else if (counter == 16 && p == 5)
+        {
+            if (o == 1 || o == 2 || o == 3 || o == 4 || o == 5)
+            {
+                printf("MASTERCARD\n");
+            }
+            else
+            {
+                printf("INVALID\n");
+            }
+        }
+        else
+        {
+            printf("INVALID\n");
+        }
     }
     else
     {
-        prodx = (b * 2) + (d * 2) + (f * 2) + (h * 2) + (j * 2) + (l * 2);
+        printf("INVALID\n");
     }
-    printf("Digit Count: %i\n", counter);
-    printf("Integer Test: ");
-    if(p > 0)
-    {
-        printf("%i",p);
-    }
-    if(o > 0)
-    {
-        printf("%i",o);
-    }
-    if(n > 0)
-    {
-        printf("%i",n);
-    }
-    printf("%i%i%i%i%i%i%i%i%i%i%i%i%i\n",m ,l, k, j, i, h, g, f, e, d, c, b, a);
-    printf("prodx: %i\n", prodx);
 }
 
-
-
-        // cc % 10 to find last digit and save to int
-        // cc * .1 to reduce by order of magnitude if cc > 9
-        // +1 counter
-        // Do while cc > 10 (maybe a different loop?)
-
-        // Multiply every second digit of cc by 2, starting with the second to last position
-        // If any product > 9, take modulo 10 of it instead, and also record a 1
-        // Add sum of results to sum of remaining digits from cc
-        // If sum % 10 = 0, card is potentially valid, proceed to identification
-
-    // Identify card type
-        // Check VISA parameters
-            // Is digit counter 13 or 16?
-            // Does first digit = 4? (digit 13 or 16 depending)
-        // Check AMEX parameters
-            // Is digit counter 15?
-            // Does digit 15 = 3?
-            // Does digit 14 = 4 or 7?
-        // Check MASTERCARD parameters
-            // Is digit counter 16?
-            // Does digit 16 = 5?
-            // Does digit 15 = 1,2,3,4 or 5?
-
-    // Print result
-
+int prodfinder(int x)
+{
+    x = x * 2;
+    if (x > 9)
+    {
+        // Modulo 10 to find ones place integer (could also just subtract 10 but that's lazy)
+        x = x % 10;
+        // Add one to simulate the tens place integer (lazy but products will never be more than 18)
+        x++;
+    }
+    return x;
+}
